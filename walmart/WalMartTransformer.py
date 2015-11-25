cb9fb46378bc8e7dd8a5498009a5b7f3aca36e1d
anabranch <wac.chambers@gmail.com>
2015-11-25 15:56:54 -0800

    small code change

--- walmart/WalMartTransformer.py
+++ walmart/WalMartTransformer.py
@@ -4,8 +4,8 @@ import numpy as np
 from sklearn.base import TransformerMixin
 from sklearn.feature_extraction import DictVectorizer
 
-class WalmartImputer(TransformerMixin):
 
+class WalmartImputer(TransformerMixin):
     def __init__(self):
         """Impute missing values.
         Columns of strings are imputed with the most frequent value (mode)
@@ -16,22 +16,28 @@ class WalmartImputer(TransformerMixin):
     def fit(self, X, y=None):
         """X: a pandas DataFrame"""
         self.fill = pd.Series([X[c].value_counts().index[0]
-            if X[c].dtype == np.dtype('O') else X[c].median() for c in X],
-            index=X.columns)
+                               if X[c].dtype == np.dtype('O') else
+                               X[c].median() for c in X],
+                              index=X.columns)
         return self
 
     def transform(self, X, y=None):
         return X.fillna(self.fill)
 
-class GWalmartTransformer(TransformerMixin):
-    def __init__(self):
-        """One hot encoder for one column."""
 
-    def fit(self, X, y=None, one_hot_column = 'DepartmentDescription', groupBy_column = 'VisitNumber'):
+class GWalmartTransformer(TransformerMixin):
+    def __init__(self,
+                 one_hot_col='DepartmentDescription',
+                 groupby_col='VisitNumber'):
+        """One hot encoder for one column."""
+        self.one_hot_col = one_hot_col
+        self.groupby_col = groupby_col
+
+    def fit(self, X, y=None):
         """X: a pandas DataFrame"""
-        self.vectorizer = DictVectorizer(sparse = False)
-        self.vectorizer.fit(X[one_hot_column].to_frame().to_dict().values())
+        self.vectorizer = DictVectorizer(sparse=False)
+        self.vectorizer.fit(X[self.one_hot_col].to_frame().to_dict().values())
         return self
 
     def transform(self, X, y=None):
-        return 
+        return
