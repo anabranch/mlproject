3dd2ba6fe17ec26d0e533f144388737e72002fcb
andyatcal <andyatcal@berkeley.edu>
2015-11-25 10:15:14 -0800

    one hot encoder added

--- walmart/WalMartTransformer.py
+++ walmart/WalMartTransformer.py
@@ -1,10 +1,9 @@
 # This file contains transformers of walmart kaggle competition data.
 import pandas as pd
 import numpy as np
-
+from sklearn.base import TransformerMixin
 
 class WalmartImputer(TransformerMixin):
-    """Inpute NAs for walmart data"""
 
     def __init__(self):
         """Impute missing values.
@@ -12,7 +11,21 @@ class WalmartImputer(TransformerMixin):
         in column.
         Columns of other types are imputed with median of column.
         """
+
     def fit(self, X, y=None):
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].median() for c in X],
+            index=X.columns)
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
+
+class WalmartOneHotEncoder(TransformerMixin):
+    def __init__(self):
+        """One hot encoder for one column."""
+
+    def fit(self, X, y=None, column_name = 'DepartmentDescription'):
         self.fill = pd.Series([X[c].value_counts().index[0]
             if X[c].dtype == np.dtype('O') else X[c].median() for c in X],
             index=X.columns)
