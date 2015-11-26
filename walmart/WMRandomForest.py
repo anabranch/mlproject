d593a8655dac6f3eeb7d4e245f07041d3a1301a3
andyatcal <andyatcal@berkeley.edu>
2015-11-25 17:06:15 -0800

    One transfomer

--- walmart/WMRandomForest.py
+++ walmart/WMRandomForest.py
@@ -6,6 +6,10 @@ import numpy as np
 from WalMartTransformer import WalmartImputer
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import train_test_split
+import datetime
 
 ####################################PreProcessing##############################################
 
@@ -18,45 +22,51 @@ train_label = raw[['TripType', 'VisitNumber']]
 train_label = train_label.groupby('VisitNumber').agg(np.mean).as_matrix()
 
 # Fill the NAs for train and test data
-imp = WalmartImputer()
-train_data = raw[['VisitNumber', 'ScanCount', 'DepartmentDescription']]
-test_data = raw_test[['VisitNumber', 'ScanCount', 'DepartmentDescription']]
-train_data = imp.fit_transform(train_data)
-test_data = imp.transform(test_data)
+train_data = raw[raw_test.columns]
+test_data = raw_test
 
-# One hot transformation for train and test DepartmentDescription
-train_data_dept = train_data[['DepartmentDescription']]
-test_data_dept = test_data[['DepartmentDescription']]
-vectorizer = DictVectorizer(sparse = False)
-train_dept_vect = vectorizer.fit_transform(train_data_dept.T.to_dict().values())
-test_dept_vect = vectorizer.transform(test_data_dept.T.to_dict().values())
+trans = GWalmartTransformer()
 
-# Fill scan count into the one hot representation 
-train_dept_scanCount = train_data[['ScanCount']].as_matrix() * train_dept_vect
-test_dept_scanCount = test_data[['ScanCount']].as_matrix() * test_dept_vect
-
-train_before_gb = np.hstack((train_data[['VisitNumber']].as_matrix(), train_dept_scanCount))
-test_before_gb = np.hstack((test_data[['VisitNumber']].as_matrix(), test_dept_scanCount))
-
-train_matrix = pd.DataFrame(train_before_gb).groupby(0).agg(np.sum).as_matrix()
-test_matrix = pd.DataFrame(test_before_gb).groupby(0).agg(np.sum).as_matrix()
+#trans.fit(train_data, group_by_col = 'VisitNumber', one_hot_cols = ['DepartmentDescription'], mul_col = 'ScanCount', fillna = True)
+train_matrix = trans.fit_transform(train_data, group_by_col = 'VisitNumber', one_hot_cols = ['DepartmentDescription'], mul_col = 'ScanCount', fillna = True)
+test_matrix = test.transform(test_data)
 
 ############################Fit and Predict with Random Forest#################################
 
-clf = RandomForestClassifier(n_estimators = 250, n_jobs = 3, random_state = 42, criterion='entropy', min_samples_split = 4, max_features = None)
-clf.fit(train_matrix, train_label.ravel())
-pred_labels = clf.predict(test_matrix)
+XTrain, XVal, yTrain, yVal = train_test_split(train_matrix, train_label, test_size=0.5, random_state=200)
+
+search_grid = {
+    "n_estimators": [100, 200, 300],
+    "max_features": ["auto"],
+    "min_samples_split": [10, 20, 5, 2],
+    "min_samples_leaf": [1],
+    "max_depth": [15, 30, 45, 60]
+}
+
+start = datetime.datetime.now()
+clf = RandomForestClassifier()
+
+model = GridSearchCV(clf, search_grid, n_jobs=3)
+model.fit(XTrain, yTrain.ravel())
+score = model.score(XVal, yVal.ravel())
+print(score)
+
+print("Total Time:", (datetime.datetime.now() - start).total_seconds())
+
+#clf = DecisionTreeClassifier()
+#clf.fit(train_matrix, train_label.ravel())
+#pred_labels = clf.predict(test_matrix)
 
 ############################Output the prediction result#######################################
 
-predictions = pd.Series(pred_labels)
-output = pd.get_dummies(predictions)
-missing_categories = set(pd.DataFrame(train_label)[0]).difference(set(output.columns))
-for missing in missing_categories:
-    output[missing] = 0
-output = output[sorted(output.columns)]
-output.columns = ["TripType_%i" % x for x in output.columns]
-output = pd.concat([pd.Series(pd.DataFrame(test_before_gb).groupby(0).agg(np.sum).index.rename("VisitNumber")), output], axis=1).astype(int)
-output.to_csv("WMP.csv", index = False)
+#predictions = pd.Series(pred_labels)
+#output = pd.get_dummies(predictions)
+#missing_categories = set(pd.DataFrame(train_label)[0]).difference(set(output.columns))
+#for missing in missing_categories:
+#    output[missing] = 0
+#output = output[sorted(output.columns)]
+#output.columns = ["TripType_%i" % x for x in output.columns]
+#output = pd.concat([pd.Series(pd.DataFrame(test_before_gb).groupby(0).agg(np.sum).index.rename("VisitNumber")), output], axis=1).astype(int)
+#output.to_csv("WMP.csv", index = False)
 
 
