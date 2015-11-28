3de96b03715b60cc32baf5791d23f3e2c1e6ffe1
anabranch <wac.chambers@gmail.com>
2015-11-28 13:49:54 -0800

    working with carry over variables, better predictions

--- walmart/factorization.py
+++ walmart/factorization.py
@@ -6,13 +6,14 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.svm import LinearSVC
 from sklearn import decomposition
 
+from kaggle_helper import KaggleHelper
 import feature_transformers as ft
 import loader
-from kaggle_helper import KaggleHelper
+import utils
 
 
 def iterate_decomps():
-    decompositions = [decomposition.PCA(), decomposition.NMF()]
+    decompositions = [decomposition.PCA()]
     estimators = []
     for dc in decompositions:
         est = run_decomposition_pipeline(dc)
@@ -21,31 +22,37 @@ def iterate_decomps():
 
 def run_decomposition_pipeline(decomp):
     kh = KaggleHelper("matrix_factorization.db")
-    clf = LinearSVC()
-    decomp = decomp
-
+    ###### DATA LOADING
     xy = loader.XY1(kh)
     X = xy['X_train']
     y = xy['y_train']
     X_val = xy['X_val']
     y_val = xy['y_val']
     X_test = xy['X_test']
+    output_index = xy['X_test_index']
+    trip_types = xy['trip_types']
+
+    print("LOADED DATA")
 
     ###### PIPELINE/CV VARIABLES
-    feat_length = X.shape[1]  # use for n_components
+    clf = LinearSVC()
+    decomp = decomp
+    fl = X.shape[1]  # use for n_components
+    num_d = 1
     cv_grid = {
-        'clf__C': np.linspace(0.5, 10, 3),
-        'decomp__n_components': np.linspace(10, feat_length, 3).astype(int)
+        'clf__C': np.linspace(0.5, 10, num_d),
+        'decomp__n_components': np.linspace(10, fl, num_d).astype(int)
     }
     num_folds = 4
 
     ####### START PREDICTIONS
+    print("TRAINING ESTIMATOR")
     pred_pipe = Pipeline(steps=[('decomp', decomp), ('clf', clf)])
     estimator = GridSearchCV(pred_pipe, cv_grid, cv=num_folds)
 
+    # DO NOT NEED TO CHANGE BEYOND THIS LINE
     kh.record_metric("validation", "start", estimator, "training", "", "")
-    estimator.fit(X, y['TripType'].values)
-
+    estimator.fit(X, y)
     kh.record_metric("validation", "end", estimator, "training", "", "")
     kh.record_metric("validation", "end", estimator, "best_params",
                      str(estimator.best_params_), "NA")
@@ -53,7 +60,16 @@ def run_decomposition_pipeline(decomp):
                      str(estimator.best_estimator_), "NA")
     kh.record_metric("validation", "end", estimator, "best_score",
                      str(estimator.best_score_), "NA")
+    validation_score = str(estimator.score(X_val, y_val))
+    kh.record_metric("validation", "end", estimator, "validation score",
+                     validation_score, "")
+    preds = estimator.predict(X_test,
+                              output_index=output_index,
+                              actual_trip_types=trip_types)
+    predictions = utils.convert_predictions(preds)
+    kh.save_test_predictions(predictions, estimator, "predictions")
     kh.end_pipeline()
+
     return estimator
 
 
