037b8965171896b3fa4e5e82bfdbe034f0598ab4
anabranch <wac.chambers@gmail.com>
2015-12-01 17:27:31 -0800

    set up command line

--- walmart/prediction_pipelines.py
+++ walmart/prediction_pipelines.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 import pandas as pd
 
@@ -131,9 +133,60 @@ def run_logistic_pipeline():
     return estimator
 
 
+def run_svc_pipeline():
+    ###### DATA LOADING
+    xy = loader.XY2(KH)  # CAN CHANGE
+
+    X = xy['X_train']
+    y = xy['y_train']
+    X_val = xy['X_val']
+    y_val = xy['y_val']
+    X_test = xy['X_test']
+    output_index = xy['X_test_index']
+    print("LOADED DATA")
+
+    ###### PIPELINE/CV VARIABLES
+    ###### DO NOT CHANGE BEFORE
+    clf = LinearSVC()
+    fl = X.shape[1]  # use for n_components
+    cv_grid = {'clf__C': np.linspace(0, 10, 8), }
+    num_folds = 3
+
+    ####### START PREDICTIONS
+    print("TRAINING ESTIMATOR")
+    pred_pipe = Pipeline(steps=[('clf', clf)])
+
+    ###### DO NOT CHANGE AFTER
+    estimator = GridSearchCV(pred_pipe, cv_grid, cv=num_folds)
+
+    # DO NOT NEED TO CHANGE BEYOND THIS LINE
+    KH.record_metric("validation", "start", estimator, "training", "", "")
+    estimator.fit(X, y)
+    KH.record_metric("validation", "end", estimator, "training", "", "")
+    KH.record_metric("validation", "end", estimator, "best_params",
+                     str(estimator.best_params_), "NA")
+    KH.record_metric("validation", "end", estimator, "best_estimator",
+                     str(estimator.best_estimator_), "NA")
+    KH.record_metric("validation", "end", estimator, "best_score",
+                     str(estimator.best_score_), "NA")
+    validation_score = str(estimator.score(X_val, y_val))
+    KH.record_metric("validation", "end", estimator, "validation score",
+                     validation_score, "")
+
+    preds = estimator.predict(X_test)
+    predictions = pd.DataFrame(
+        {"VisitNumber": output_index,
+         "TripType": preds})
+    KH.save_test_predictions(utils.convert_predictions(predictions), estimator,
+                             "predictions")
+    KH.end_pipeline()
+
+    return estimator
+
+
 def run_random_forest_pipeline():
     ###### DATA LOADING
-    xy = loader.XY3(KH)  # CAN CHANGE
+    xy = loader.XY2(KH)  # CAN CHANGE
 
     X = xy['X_train']
     y = xy['y_train']
@@ -190,7 +243,7 @@ def run_random_forest_pipeline():
 
 def run_knn_pipeline():
     ###### DATA LOADING
-    xy = loader.XY4(KH)  # CAN CHANGE
+    xy = loader.XY5(KH)  # CAN CHANGE
 
     X = xy['X_train']
     y = xy['y_train']
@@ -244,4 +297,16 @@ def run_knn_pipeline():
 
 
 if __name__ == '__main__':
-    run_knn_pipeline()
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('pipeline')
+    args = parser.parse_args()
+    if args.pipeline == 'knn':
+        run_knn_pipeline()
+    elif args.pipeline == 'rf':
+        run_random_forest_pipeline()
+    elif args.pipeline == 'logistic':
+        run_logistic_pipeline()
+    elif args.pipeline == 'svc':
+        run_svc_pipeline()
+    elif args.pipeline == 'decomps':
+        iterate_decomps()
