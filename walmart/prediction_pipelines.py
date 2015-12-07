0b16d3b24f1412ed55da7866b6704d5a8dbb0cf8
andyatcal <andyatcal@berkeley.edu>
2015-12-07 15:12:03 -0800

    extra_tree_pipeline added

--- walmart/prediction_pipelines.py
+++ walmart/prediction_pipelines.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, ExtraTreesClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import decomposition
 
@@ -240,6 +240,59 @@ def run_gradient_boosting_pipeline():
 
     return estimator
 
+def run_extra_trees_pipeline():
+    ###### DATA LOADING
+    xy = XYLOADER(KH)  # CAN CHANGE
+
+    X = xy['X_train']
+    y = xy['y_train']
+    X_val = xy['X_val']
+    y_val = xy['y_val']
+    X_test = xy['X_test']
+    output_index = xy['X_test_index']
+    print("LOADED DATA")
+
+    ###### PIPELINE/CV VARIABLES
+    ###### DO NOT CHANGE BEFORE
+    clf = ExtraTreesClassifier()
+    fl = X.shape[1]  # use for n_components
+    cv_grid = {
+        "clf__n_estimators": [100, 200],
+        "clf__min_samples_split": [10, 20, 50],
+        "clf__min_samples_leaf": [5, 15, 35, 60]
+    }
+    num_folds = 3
+
+    ####### START PREDICTIONS
+    print("TRAINING ESTIMATOR")
+    pred_pipe = Pipeline(steps=[('clf', clf)])
+
+    ###### DO NOT CHANGE AFTER
+    estimator = GridSearchCV(pred_pipe, cv_grid, cv=num_folds)
+
+    # DO NOT NEED TO CHANGE BEYOND THIS LINE
+    KH.record_metric("validation", "start", estimator, "training", "", "")
+    estimator.fit(X, y)
+    KH.record_metric("validation", "end", estimator, "training", "", "")
+    KH.record_metric("validation", "end", estimator, "best_params",
+                     str(estimator.best_params_), "NA")
+    KH.record_metric("validation", "end", estimator, "best_estimator",
+                     str(estimator.best_estimator_), "NA")
+    KH.record_metric("validation", "end", estimator, "best_score",
+                     str(estimator.best_score_), "NA")
+    validation_score = str(estimator.score(X_val, y_val))
+    KH.record_metric("validation", "end", estimator, "validation score",
+                     validation_score, "")
+
+    preds = estimator.predict(X_test)
+    predictions = pd.DataFrame(
+        {"VisitNumber": output_index,
+         "TripType": preds})
+    KH.save_test_predictions(utils.convert_predictions(predictions), estimator,
+                             "predictions")
+    KH.end_pipeline()
+
+    return estimator
 
 def run_random_forest_pipeline():
     ###### DATA LOADING
