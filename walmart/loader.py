c9b70f0276fede0039e246e81ed086793ecf939c
anabranch <wac.chambers@gmail.com>
2015-12-07 17:02:46 -0800

    NO LONGER DOING A TRAIN TEST SPLIT

--- walmart/loader.py
+++ walmart/loader.py
@@ -30,14 +30,14 @@ def autosplit(func):
         val = func(*args, **kwargs)
         X = val['X']
         y = val['y']
-        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+        # X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
 
         assert X.shape[1] == val['X_test'].shape[1]
         return {
             "X_train": X_train,
             "y_train": y_train,
-            "X_val": X_val,
-            "y_val": y_val,
+            "X_val": X_train,
+            "y_val": y_train,
             "X_test": val['X_test'],
             "X_test_index": val['X_test_index']
         }
